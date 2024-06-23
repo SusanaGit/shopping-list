a6fa1e6c56ab738e190a9ed08ba760ec5c1c7bf9
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-06-23 19:15:33 +0200

    25. Añado un icono al botón de la vista Tab2Page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton} from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonInput,
+  IonButton,
+  IonIcon
+} from '@ionic/angular/standalone';
 import {FormsModule} from "@angular/forms";
+import { addIcons } from 'ionicons';
+import { addOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, FormsModule, IonButton]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, FormsModule, IonButton, IonIcon]
 })
 export class Tab2Page {
 
   public item: string;
 
-  constructor() {}
+  constructor() {
+    addIcons({addOutline})
+  }
 
   addItem() {
     console.log("Item introducido por el usuario: ", this.item);
