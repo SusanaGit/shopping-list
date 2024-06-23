662184fd722d9098b5e5d3005a0d0ac70be8f51a
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-06-23 18:28:35 +0200

    20. Añado a la vista Tab2Page un <ion-input> para que el usuario pueda escribir

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput]
 })
 export class Tab2Page {
 
