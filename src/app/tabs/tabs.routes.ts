5bef1afd980ab08eaccbb49e7ee689ba029a7be3
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-06-23 15:18:38 +0200

    06. Eliminamos el tab3

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -17,11 +17,6 @@ export const routes: Routes = [
           import('../tab2/tab2.page').then((m) => m.Tab2Page),
       },
       {
-        path: 'tab3',
-        loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
-      },
-      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full',
