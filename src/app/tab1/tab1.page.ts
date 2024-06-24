a9d4c202c271ae2487475a04341cca958f690b55
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-06-24 13:23:44 +0200

    31. Hago que muestre el mensaje “No hay items en tu lista” cuando la lista esté vacía

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel} from '@ionic/angular/standalone';
 import {ShoppingItemsService} from "../services/shopping-items.service";
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel],
 })
 export class Tab1Page {
   constructor(public shoppingList: ShoppingItemsService) {}
